385f254e66007ed313845b94c4e903c94a5b52e4
Intern Rayhan <rayhan.shellbeehaken@gmail.com>
2025-01-06 12:26:19 +0600

    Code refactoring for Dice.jsx

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import {show, hide} from '../features/lock/lockSlice';
-import {showButton, hideButton, disableButton, enableButton} from '../features/button/buttonSlice';
+import { useDispatch } from 'react-redux'
+import {enableButton} from '../features/button/buttonSlice';
 import './dice-module.css';
 
 const Dice = ({dice}) => {
     const dices = ['/assets/dice_1.png', '/assets/dice_2.png', '/assets/dice_3.png', '/assets/dice_4.png', '/assets/dice_5.png', '/assets/dice_6.png'];
     const [index, setIndex] = useState(0);
-
     const dispatch = useDispatch();
 
-
-   const [roll, setRoll] = useState(true);
-
     useEffect(() => {
-        let pos = 0;
-        
+        let pos = 0; 
         const interval = setInterval(() => {
-
             if(pos < dice.rollIndexes.length){
                 setIndex(dice.rollIndexes[pos]);
                 pos += 1;
@@ -28,7 +21,7 @@ const Dice = ({dice}) => {
                 clearInterval(interval);
             }
         }, 300);
-
+        
         return () => {
             clearInterval(interval);
         }
